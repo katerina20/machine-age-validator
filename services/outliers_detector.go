948333209e0c4e5c6aa374c2958d2473b846cba5
Kateryna Oblakevych <malutina.catya14@gmail.com>
2024-01-01 17:17:25 +0200

    Fix IQR algorithm.

--- services/outliers_detector.go
+++ services/outliers_detector.go
@@ -17,44 +17,19 @@ func DetectOutliers(machines []models.Machine) ([]models.Machine, error) {
 	}
 
 	sort.Ints(ages)
-
-	q1, q3 := calculateQuartiles(ages)
+	q1 := ages[len(ages)/4]
+	q3 := ages[3*len(ages)/4]
 	iqr := q3 - q1
 
-	lowerBound := q1 - int(1.5*float64(iqr))
-	upperBound := q3 + int(1.5*float64(iqr))
+	iqrMultiplier := 2.0
+	upperBound := q3 + int(iqrMultiplier*float64(iqr))
 
 	var outliers []models.Machine
 	for _, m := range machines {
 		age, _ := utils.ParseToDays(m.Age)
-		if age < lowerBound || age > upperBound {
+		if age > upperBound {
 			outliers = append(outliers, m)
 		}
 	}
-
 	return outliers, nil
-}
-
-func calculateQuartiles(data []int) (int, int) {
-	n := len(data)
-	mid := n / 2
-
-	var q1, q3 int
-	if n%2 == 0 {
-		q1 = median(data[:mid])
-		q3 = median(data[mid:])
-	} else {
-		q1 = median(data[:mid])
-		q3 = median(data[mid+1:])
-	}
-
-	return q1, q3
-}
-
-func median(data []int) int {
-	n := len(data)
-	if n%2 == 0 {
-		return (data[n/2-1] + data[n/2]) / 2
-	}
-	return data[n/2]
 }
